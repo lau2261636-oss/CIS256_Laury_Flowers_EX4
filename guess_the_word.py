8b6f65ffba4deb55bc2ae976135e72deb7449c55
lau2261636-oss <lau2261636@maricopa.edu>
2026-04-21 22:28:12 -0700

    Create guess_the_word.py
    Added guess game

---
+++ guess_the_word.py
@@ -0,0 +1,59 @@
+# Laury Flowers
+# CIS256 Spring 2026
+# Exercise Assignment 4
+
+import random
+
+# list of words
+words = ["apple", "banana", "grape", "orange"]
+
+# pick random word
+def get_word():
+    return random.choice(words)
+
+# check if guess is in word
+def check_guess(word, guess):
+    return guess in word
+
+
+def play_game():
+    word = get_word()
+    guessed = []
+    attempts = 6
+
+    print("Guess the word!")
+
+    while attempts > 0:
+        display = ""
+
+        for letter in word:
+            if letter in guessed:
+                display += letter
+            else:
+                display += "_"
+
+        print(display)
+
+        if display == word:
+            print("You win!")
+            return
+
+        guess = input("Enter a letter: ")
+
+        if guess in guessed:
+            print("You already guessed that letter")
+            continue
+
+        guessed.append(guess)
+
+        if check_guess(word, guess):
+            print("Correct!")
+        else:
+            print("Wrong!")
+            attempts -= 1
+
+    print("You lost! The word was:", word)
+
+
+if __name__ == "__main__":
+    play_game()
